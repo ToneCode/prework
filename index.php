20e829bb1912555505d3070ac6a3932443558992
Anthony Williams <pull.rebase>
2016-04-13 13:10:45 -0600

    Added test page to ensure PhpStorm is setup

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
 	<body>
 		<p>I completed the prework!</p>
 		<p>I'm learning all about programming in this class</p>
-		<a href="https://www.DeepDivecoding.com">link to class bootcamp</a>
+		<a href="http://stemuluscenter.org/deep-dive-coding-bootcamp/">link to class bootcamp</a>
 	</body>
 </html>
